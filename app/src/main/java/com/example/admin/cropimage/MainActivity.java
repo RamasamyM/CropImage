901faa68549a9daf14f6243c6b6e84fe29f52249
ramasamy11 <ramasamy.vcp@gmail.com>
2016-03-30 14:14:57 +0530

    changed by  cl

--- app/src/main/java/com/example/admin/cropimage/MainActivity.java
+++ app/src/main/java/com/example/admin/cropimage/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.admin.cropimage;
 
 import android.content.ActivityNotFoundException;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,6 +9,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +20,9 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * Created by Admin on 3/17/2016.
@@ -37,6 +42,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String selectedImagePath;
     String mCurrentPhotoPath;
     private Uri picUri;
+    String FILENAME = "pic.jpg";
+    String string = "hello world!";
+    Context context;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,7 +67,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case get_image_action:
                 Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                File file = new File(getFilesDir(),"pic.jpg");
+                File file= new File(Environment.getExternalStorageDirectory(),FILENAME);
+                FileOutputStream fos = null;
+                try {
+                    fos = openFileOutput(FILENAME, Context.MODE_PRIVATE);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.write(string.getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, file);
                 startActivityForResult(cameraIntent, CAMERA_REQUEST);
             default:
@@ -83,7 +108,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         });
         builder.show();
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+    public void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
         if (resultCode == RESULT_OK) {
             if (requestCode == SELECT_PICTURE) {
                 picUri = data.getData();
@@ -94,15 +120,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 int columnIndex = c.getColumnIndex(filePath[0]);
                 String picturePath = c.getString(columnIndex);
                 c.close();
-                Bitmap thumbnail = (BitmapFactory.decodeFile(picturePath));
+                photo = (BitmapFactory.decodeFile(picturePath));
                 Log.w("image path ...", picturePath + "");
-                cameraImage.setImageBitmap(thumbnail);
-            } else if (requestCode == CAMERA_REQUEST) {
+                cameraImage.setImageBitmap(photo);
+            } else if (requestCode == CAMERA_REQUEST)
+            {
                 Bundle bundle = data.getExtras();
                 if (bundle != null) {
                     photo = (Bitmap)
                             bundle.get("data");
-                    cameraImage.setImageBitmap(photo);
+                   /* cameraImage.setImageBitmap(photo);*/
+                    autoCrop();
                 }
             } else if (requestCode == PIC_CROP) {
                 Log.i("MainActvity ", "request code " + PIC_CROP);
@@ -115,7 +143,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
     }
-    private void performCrop() {
+    private void performCrop()
+    {
         try {
             Intent cropIntent = new Intent("com.android.camera.action.CROP");
             cropIntent.setDataAndType(picUri, "image/*");
@@ -125,6 +154,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             cropIntent.putExtra("outputX", 256);
             cropIntent.putExtra("outputY", 256);
             cropIntent.putExtra("return-data", true);
+            cameraImage.setImageBitmap(photo);
             startActivityForResult(cropIntent, PIC_CROP);
         }
         catch (ActivityNotFoundException anfe)
@@ -134,10 +164,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             toast.show();
         }
     }
-// don't need  free crop can we  use this  method
-    private void performCrop1() {
+    // don't need  free crop can we  use this  method
+    private void autoCrop() {
         Bitmap croppedBmp = Bitmap.createBitmap(photo, 0, 0, photo.getWidth(), photo.getHeight() - 10);
-        cameraImage1.setImageBitmap(croppedBmp);
+        cameraImage.setImageBitmap(croppedBmp);
     }
+
 }
 
